c07899b0149064b1fa2a208af8ba7a71dfddf968
Raynixx <94186653+x4raynixx@users.noreply.github.com>
2025-06-16 17:15:03 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-red-500 flex justify-center items-center">
+      <h1 className="text-white text-4xl font-bold">If you see red screen<br></br>Tailwind CSS Is successfully installed!</h1>
+    </div>
+  )
+}
